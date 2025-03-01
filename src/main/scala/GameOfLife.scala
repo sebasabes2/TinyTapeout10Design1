5a364df37b219b27b4c050219b67c09ee6df0495
Sebastian <sebastian@familienholdt.dk>
2025-03-01 23:51:47 +0100

    Working with game of life logic

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -48,12 +48,15 @@ class GameOfLife() extends Module {
 
   // Finite State Machine
   object States extends ChiselEnum {
-    val reset, display, update, process, begincopy, copy, input = Value
+    val reset, display, update, beginCountNeighbours, countNeighbours, decideState, begincopy, copy, input = Value
   }
   val state = RegInit(States.reset)
   val xIndex = RegInit(0.U(3.W))
   val yIndex = RegInit(0.U(3.W))
   val updateCounter = Reg(UInt(5.W))
+  val neighbours = Reg(UInt(3.W))
+  val dx = Reg(UInt(2.W))
+  val dy = Reg(UInt(2.W))
 
   switch (state) {
     is (States.reset) {
@@ -82,24 +85,62 @@ class GameOfLife() extends Module {
       when (updateCounter === 0.U && io.tick) {
         xIndex := 0.U
         yIndex := 0.U
-        cellMem.io.address := 0.U
-        state := States.process
+        // cellMem.io.address := 0.U
+
+        // neighbours := 0.U
+        // dx := 1.U
+        // dy := 0.U
+        // cellMem.io.address := (y - 1.U) ## (x - 1.U)
+        // state := States.countNeighbours
+        state := States.beginCountNeighbours
       } .otherwise {
         state := States.input
       }
     }
 
-    is (States.process) {
+    is (States.beginCountNeighbours) {
+      neighbours := 0.U
+      dx := 1.U
+      dy := 0.U
+      cellMem.io.address := (yIndex - 1.U) ## (xIndex - 1.U)
+      state := States.countNeighbours
+    }
+
+    is (States.countNeighbours) {
+      when (!dx(1)) {
+        dx := dx + 1.U
+      } .otherwise {
+        dx := 0.U
+        when (!dy(1)) {
+          dy := dy + 1.U
+        } .otherwise {
+          dy := 0.U
+        }
+      }
+      cellMem.io.address := (yIndex + dy - 1.U) ## (xIndex + dx - 1.U)
+      when (cellMem.io.dataRead) {
+        neighbours := neighbours + 1.U
+      }
+      when (dx === 0.U && dy === 0.U) {
+        cellMem.io.address := yIndex ## xIndex
+        state := States.decideState
+      }
+    }
+
+    is (States.decideState) {
       val index = yIndex ## xIndex
       val nextIndex = index + 1.U
       yIndex := nextIndex(5,3)
       xIndex := nextIndex(2,0)
       when (nextIndex === 48.U) {
         state := States.begincopy
+      } .otherwise {
+        state := States.beginCountNeighbours
       }
-      cellMem.io.address := nextIndex
+      val alive = cellMem.io.dataRead
+      val newState = alive && (neighbours === 3.U || neighbours === 4.U) || !alive && (neighbours === 3.U)
       copyMem.io.address := index
-      copyMem.io.dataWrite := ~cellMem.io.dataRead
+      copyMem.io.dataWrite := newState
       copyMem.io.writeEnabled := true.B
     }
 
